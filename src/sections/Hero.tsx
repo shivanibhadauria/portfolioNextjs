0653f4ef793fee258ab44a8729d8860afae5d08e
Shivani Bhadauria <shivanibhadauria921997@gmal.com>
2024-12-18 20:58:49 +0530

    responsivness added in hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import Arrowdown from "../assets/icons/arrow-down.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md-py-48 lg:py-60 ">
       <div className="container">
         <div className=" flex flex-col items-center">
           <Image className="size-[100px]  " src={mememojiImage} alt="memoji" />
@@ -14,16 +14,19 @@ export const HeroSection = () => {
               Available for new projects
             </div>
           </div>
-          <h1 className=" text-white font-serif text-3xl mt-6 tracking-wide  text-center   ">
-            Building Exceotional User Experiences
-          </h1>
-          <p className="mt-4 text-center text-white/60 ">
-            I specialize in transforming design into functional, high-performing
-            web applications. Lets discuss your next project
-          </p>
 
-          <div className="flex  flex-col mt-4 gap-4 items-center">
-            <button className=" border-white/15 border  rounded-xl inline-flex items-center gap-2 mt-4  h-12 px-6 ">
+          <div className=" max-w-lg mx-auto ">
+            <h1 className=" text-white font-serif text-3xl md:text-5xl  mt-8 tracking-wide  text-center   ">
+              Building Exceptional User Experiences
+            </h1>
+            <p className="mt-4 text-center text-white/60 md:text-lg ">
+              I specialize in transforming design into functional,
+              high-performing web applications. Lets discuss your next project
+            </p>
+          </div>
+
+          <div className="flex max-md:flex-col mt-4 gap-4 items-center">
+            <button className=" border-white/15 border  rounded-xl inline-flex items-center gap-2   h-12 px-6 ">
               <span className="font-semibold">Explore my work</span>
               <Arrowdown className="size-4  " />
             </button>
