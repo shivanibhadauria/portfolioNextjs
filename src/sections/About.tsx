0937d65286fcbd75e059e7b660da5a25b7865118
shivanibhadauria <shivanibhadauria921997@gmail.com>
2025-09-10 22:01:00 +0530

    style(About): fix whitespace in gradient div className

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -179,7 +179,7 @@ export const AboutSection = () => {
                 />
 
                 <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  rounded-full  after:content-[''] after:absolute   after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30  ">
-                  <div className=" absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s] "></div>
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s] "></div>
                   <div className=" absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10 "></div>
                   <Image src={me} alt="smile" className="size-20 rounded-full object-cover" />
                 </div>
